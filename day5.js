e02752d111b65b328df54766471b117ce907f829
syedsajeed6969 <syedsajeed6969@gmail.com>
2025-10-14 22:34:33 +0530

    m

---
+++ day5.js
@@ -0,0 +1,15 @@
+// Lets learn shift, unshift, push, pop
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.shift(); // removes the first element from the array
+
+
+// fruits.unshift('new-item1');  // add the element at the begining of the array
+// fruits.unshift('new-item2');
+
+fruits.push('new-fruit') // add the element at the end of array
+
+fruits.pop() // remove the element from elemnt 
+ 
+
+
+console.log(fruits);
