ff03c72a52f47978efcfb8b597f264b9c0e38b72
syedsajeed6969 <syedsajeed6969@gmail.com>
2025-11-25 23:16:03 +0530

    selection sort

--- topics/sortingAlgo/practice.js
+++ topics/sortingAlgo/practice.js
@@ -1,5 +1,5 @@
 // bubble sort
-// SORT ARRAY IN ASCENDING ORDER 
+// SORT ARRAY IN ASCENDING ORDER
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
@@ -11,4 +11,21 @@ function bubbleSort(arr) {
   return arr;
 }
 
-console.log(bubbleSort([12, 18, 13, 11, 15, 20]));
+// console.log(bubbleSort([12, 18, 13, 11, 15, 20]));
+
+// selection sort
+// SORT ARRAY IN ASCENDING ORDER
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length-1; i++) {
+    let small = i;
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[small]) {
+        small = j;
+      }
+    }
+    [arr[i], arr[small]] = [arr[small], arr[i]];
+  }
+  return arr;
+}
+
+console.log(selectionSort([12, 18, 13, 11, 15, 20]));
