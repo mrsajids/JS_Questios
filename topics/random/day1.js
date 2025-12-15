b535ff29526821bc9618ceb98a6f107a9c49d51a
syedsajeed6969 <syedsajeed6969@gmail.com>
2025-12-15 22:10:22 +0530

    common prefix problem

--- topics/random/day1.js
+++ topics/random/day1.js
@@ -64,27 +64,52 @@ Example 2:
 Input: s = "", t = "y"
 Output: "y" 
 */
-var findTheDifference = function (s, t) {
-  let map = new Map();
+// var findTheDifference = function (s, t) {
+//   let map = new Map();
 
-  for (let i = 0; i < s.length; i++) {
-    map.set(s[i], (map.get(s[i]) || 0) + 1);
-  }
+//   for (let i = 0; i < s.length; i++) {
+//     map.set(s[i], (map.get(s[i]) || 0) + 1);
+//   }
 
-  for (let i = 0; i < t.length; i++) {
-    if (map.has(t[i])) {
-      map.set(t[i], map.get(t[i]) - 1);
-      if (map.get(t[i]) === 0) map.delete(t[i]);
-    } else {
-      // This is the extra char
-      return t[i];
+//   for (let i = 0; i < t.length; i++) {
+//     if (map.has(t[i])) {
+//       map.set(t[i], map.get(t[i]) - 1);
+//       if (map.get(t[i]) === 0) map.delete(t[i]);
+//     } else {
+//       // This is the extra char
+//       return t[i];
+//     }
+//   }
+
+//   // Whatever remains in the map is the extra char
+//   for (let [char] of map) {
+//     return char;
+//   }
+// };
+
+// console.log(findTheDifference("abcd", "abcdef"));
+
+// Longest Common Prefix
+/* 
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+*/
+
+function getCommonPrefix(strs) {
+  let temp =""
+  for (let i = 0; i < strs.length; i++) {
+    const word = strs[0];
+     
+    if(strs[1]?.[i].startsWith(word[i]) && strs[2]?.[i].startsWith(word[i])){
+      temp = temp + word[i]
     }
+    
   }
-
-  // Whatever remains in the map is the extra char
-  for (let [char] of map) {
-    return char;
-  }
-};
-
-console.log(findTheDifference("abcd", "abcdef"));
+  return temp;
+}
+console.log(getCommonPrefix(["flower", "flow", "flight"]));
